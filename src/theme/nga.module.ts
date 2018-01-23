6534889b5b97efbc5fc9f11facc083aa5063c6bb
LG <GL717@outlook.com>
2018-01-23 15:20:53 +0800

    generate common component

---
+++ src/theme/nga.module.ts
@@ -0,0 +1,19 @@
+/**
+ * Created by gaole on 2018/1/23.
+ */
+import {NgModule} from "@angular/core";
+import {NgaComponent} from "./nga.component";
+import {Name2AvatarPipe} from "./pipes/name2Avatar/name2Avatar.pipe";
+
+@NgModule({
+  declarations:[
+    NgaComponent,
+    Name2AvatarPipe
+  ],
+  imports:[],
+  exports:[
+    Name2AvatarPipe
+  ]
+})
+
+export class NgaModule{}
