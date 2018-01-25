2407cd9e4cfe0898b566fdf332fe4ad3158c2683
LG <GL717@outlook.com>
2018-01-25 13:49:00 +0800

    generate person page

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {LoginPage} from "../login/login";
 
 @Component({
   selector: 'page-contact',
@@ -11,4 +12,7 @@ export class ContactPage {
 
   }
 
+  logout(){
+    this.navCtrl.push(LoginPage);
+  }
 }
